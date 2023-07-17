c00e8a7f17249f27141ac6759f93284fc0d97331
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-07-17 19:04:36 +0330

    core: add local vs global scope demo

---
+++ core/local-vs-global-scope.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+var a = 6
+
+func main() {
+	// Since we are using a as a local variable, mutating it will not affect the
+	// a variable in global scope
+	fmt.Println("Using local variable...")
+	print()
+	demoLocal()
+	print()
+
+	// Using the a variable in global scope context mutates it
+	fmt.Println("Using global variable...")
+	print()
+	demoGlobal()
+	print()
+}
+
+// Just print the a variable
+func print() {
+	fmt.Println(a)
+}
+
+/*
+** Following two functions are almost the same, but introduction of := instead
+** of = in demoLocal function identified the a as a local variable withing the
+** function so the assignment won't affect the a variable in global scope.
+**
+** It can be a source of errors, so watch out
+ */
+
+func demoLocal() {
+	a := 5
+	fmt.Println(a)
+}
+
+func demoGlobal() {
+	a = 5
+	fmt.Println(a)
+}
