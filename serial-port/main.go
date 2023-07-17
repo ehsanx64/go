090a8157ab82886fb855df739e93d0fed99756ca
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-07-18 00:07:15 +0330

    serial-port: add feature to send files over uart - wip

--- serial-port/main.go
+++ serial-port/main.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 
@@ -13,9 +14,11 @@ import (
 )
 
 const (
-	defaultPort = "/dev/ttyUSB0"
+	defaultPort     = "/dev/ttyUSB0"
+	defaultFirmware = "nodemcu"
 )
 
+// NodeMCU (lua)
 func nodemcuComm() {
 	buffer := make([]byte, 100)
 	commands := map[string]string{
@@ -62,6 +65,15 @@ func punyforthComm() {
 		"simple_arith": `
 			25 2 * 3 * print: "Result: " . cr
 		` + " \n\r",
+		"delay_demo": `
+			3000 ms 
+			152 load
+			+led 1000 ms -led
+			create: p LED c,
+			p 1 pwm-init 1023 pwm-freq 1000 pwm-duty pwm-start
+			1000 ms
+			1000 pwm-duty
+		` + " \n\r",
 	}
 
 	mode := &serial.Mode{
@@ -80,7 +92,7 @@ func punyforthComm() {
 	fmt.Println("Port", defaultPort, "is opened")
 
 	// port.Write requires a []byte so cast the string
-	readBytes, err := port.Write([]byte(commands["simple_arith"]))
+	readBytes, err := port.Write([]byte(commands["delay_demo"]))
 	if err != nil {
 		fmt.Println("Error writing:", err)
 	} else {
@@ -96,8 +108,66 @@ func punyforthComm() {
 		}
 	}
 }
+
+// Send the given forth file in current directory to default port
+func sendForthFile(filename string) {
+	buffer := make([]byte, 100)
+	contents, err := ioutil.ReadFile(filename + ".fs")
+	if err != nil {
+		panic(err)
+	}
+
+	mode := &serial.Mode{
+		BaudRate: 115200,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+
+	port, err := serial.Open(defaultPort, mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer port.Close()
+
+	fmt.Println("Port", defaultPort, "is opened")
+
+	// port.Write requires a []byte so cast the string
+	readBytes, err := port.Write([]byte(contents))
+	if err != nil {
+		fmt.Println("Error writing:", err)
+	} else {
+		fmt.Println("Wrote", readBytes, "bytes")
+		os.Exit(1)
+	}
+
+	for {
+		count, err := port.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading:", err)
+		} else if count != 0 {
+			fmt.Println(">>>", string(buffer[:count]))
+		}
+	}
+}
+
 func main() {
 	var defaultPortFound bool = false
+	var shouldSendFile bool = false
+	var infile string
+
+	if len(os.Args) > 1 {
+		shouldSendFile = true
+	}
+
+	// Display argument details if there are any
+	if shouldSendFile {
+		infile = os.Args[1]
+
+		fmt.Println("Program:", os.Args[0])
+		fmt.Println("Input file:", infile)
+		fmt.Println("Arg count:", len(os.Args))
+	}
 
 	ports, err := enumerator.GetDetailedPortsList()
 	if err != nil {
@@ -126,8 +196,16 @@ func main() {
 	}
 
 	if defaultPortFound {
-		//nodemcuComm()
-		punyforthComm()
+		if shouldSendFile {
+			sendForthFile(infile)
+		} else {
+			switch defaultFirmware {
+			case "nodemcu":
+				nodemcuComm()
+			default:
+				punyforthComm()
+			}
+		}
 	} else {
 		fmt.Println("Default port not found. Exiting...")
 	}
