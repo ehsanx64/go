454d283a704fc56697011935245622f5ca1dac31
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-07-31 16:28:38 +0330

    core: add defer example

---
+++ core/defer.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/*
+** Deferred expressions resumed based on their proximity not their
+** order of declaration.
+ */
+func demoDeferOrder() {
+	func() {
+		fmt.Println("1")
+	}()
+
+	defer func() {
+		fmt.Println("2")
+
+		defer func() {
+			fmt.Println("2:1")
+		}()
+
+		func() {
+			fmt.Println("2:2")
+		}()
+
+		defer func() {
+			fmt.Println("2:3")
+		}()
+
+		func() {
+			fmt.Println("2:4")
+		}()
+
+		func() {
+			fmt.Println("2:5")
+		}()
+
+		defer func() {
+			fmt.Println("2:6")
+		}()
+
+		func() {
+			fmt.Println("2:7")
+		}()
+	}()
+
+	func() {
+		fmt.Println("3")
+	}()
+}
+
+/*
+** State change using deffered calls is done this way
+ */
+func deferredStateChange() {
+	fmt.Println("# Deferred Expressions")
+
+	x := 0
+
+	dumpX := func() {
+		fmt.Printf("x: %d\n", x)
+	}
+
+	defer dumpX()
+
+	defer func() {
+		x = 4
+	}()
+
+	defer dumpX()
+
+	dumpX()
+}
+
+/*
+** Deferred calls are executed in a queue. A sleeping (or blocked)
+** function can block the rest of deferred function in the queue
+ */
+func deferredSleeps() {
+	fmt.Println("# Deferred Sleeps")
+
+	x := 0
+
+	dumpX := func() {
+		fmt.Printf("x: %d\n", x)
+	}
+
+	defer func() {
+		fmt.Println("Last")
+	}()
+
+	// This deferred call and the above one will be blocked by
+	// the sleepy one
+	defer dumpX()
+
+	// The Sleepy One
+	defer func() {
+		time.Sleep(2 * time.Second)
+		x = 4
+	}()
+
+	defer dumpX()
+
+	dumpX()
+
+}
+
+func main() {
+	//demoDeferOrder()
+
+	//deferredStateChange()
+
+	deferredSleeps()
+}
