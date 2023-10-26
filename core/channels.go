72ea15ea7926e724def261d7b912b63d4f9c104f
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-10-26 18:54:19 +0330

    core: add non-blocking channel operations example

--- core/channels.go
+++ core/channels.go
@@ -123,6 +123,50 @@ func demoTimeout() {
 	}
 }
 
+/*
+** Basic sends and receives on channels are blocking. However, we can use select
+** with a default clause to implement non-blocking sends, recieves, and even
+** non-blocking multi-way selects.
+**
+ */
+func demoNonblockingOps() {
+	messages := make(chan string)
+	signals := make(chan bool)
+
+	// Here's a non-blocking recieve. If a value is available on messages then
+	// select will take the <-messages case with that value. If not it immediately
+	// take the default case.
+	select {
+	case msg := <-messages:
+		fmt.Println("recieve message", msg)
+	default:
+		fmt.Println("No message received")
+	}
+
+	// A non-blocking send works similarly. Here msg cannot be sent to the
+	// messages channel, because the channel has no buffer and there is no
+	// receiver. Therefore the default case is selected.
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("Sent message", msg)
+	default:
+		fmt.Println("No message sent")
+	}
+
+	// We can use multiple cases above the default clause to implement a
+	// multi-way non-blocking select. Here we attempt non-blocking receives on
+	// both messages and signals.
+	select {
+	case msg := <-messages:
+		fmt.Println("Receive message", msg)
+	case sig := <-signals:
+		fmt.Println("Receive signal", sig)
+	default:
+		fmt.Println("No activity")
+	}
+}
+
 // The ping function only accepts a channel for sending values. Trying to receive
 // on the channel would cause a compile-time error.
 func ping(pings chan<- string, msg string) {
@@ -150,5 +194,8 @@ func main() {
 	//demoChannelDirections()
 
 	// Timeout demo
-	demoTimeout()
+	//demoTimeout()
+
+	// Non-blocking demo
+	demoNonblockingOps()
 }
