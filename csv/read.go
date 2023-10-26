e9378828ea97c82f611817d4a470e69de5893d60
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-10-26 19:58:10 +0330

    csv: add example to read a csv file line by line

---
+++ csv/read.go
@@ -0,0 +1,46 @@
+/*
+** Read a .csv file in a line-by-line fashion
+ */
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const theFile = "persons.csv"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func main() {
+	// Open the file
+	f, err := os.Open(theFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Close the file in the end
+	defer f.Close()
+
+	// Read values using csv.Reader
+	csvReader := csv.NewReader(f)
+	for {
+		rec, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Dump the read line
+		fmt.Printf("%+v\n", rec)
+	}
+}
