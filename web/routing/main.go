901c408a7e18a92e33ec5053790e1f5bd624f021
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-11-17 19:41:16 +0330

    web: add test for other http verbs in routing example

--- web/routing/main.go
+++ web/routing/main.go
@@ -7,6 +7,34 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	fmt.Fprintf(w, "Getting the book...\n")
+	fmt.Fprintf(w, "Title: %s\n", vars["title"])
+}
+
+func AddBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	fmt.Fprintf(w, "Adding the book...\n")
+	fmt.Fprintf(w, "Title: %s\n", vars["title"])
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	fmt.Fprintf(w, "Updating the book...\n")
+	fmt.Fprintf(w, "Title: %s\n", vars["title"])
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	fmt.Fprintf(w, "Deleting the book...\n")
+	fmt.Fprintf(w, "Title: %s\n", vars["title"])
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -17,6 +45,12 @@ func main() {
 
 		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
 	})
+
+	// Books handlers (http verbs)
+	r.HandleFunc("/books/{title}", GetBook).Methods("GET")
+	r.HandleFunc("/books/{title}", AddBook).Methods("POST")
+	r.HandleFunc("/books/{title}", UpdateBook).Methods("PUT")
+	r.HandleFunc("/books/{title}", DeleteBook).Methods("DELETE")
 
 	http.ListenAndServe(":8080", r)
 }
