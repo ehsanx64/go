77cda6dbfa7cfc00697db40a05925cb8ec24de17
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-11-18 14:31:45 +0330

    core: add tests for the any type and some comments to the types example

--- core/types.go
+++ core/types.go
@@ -1,20 +1,136 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/cmplx"
 	"reflect"
+	"strings"
 )
 
 var x interface{} = []int{1, 2, 3}
 var y interface{} = 2.3
 
+const tableWidth = 17
+
 func main() {
+	// Identify types using printf specifiers
 	idByPrintSpecifiers()
+
+	// Identify types using switch
 	idBySwitch()
+
+	// Identify types by reflection
 	idByReflection()
+
+	// Simple types demo
 	demoTypes()
+
+	// Type inference demo
 	demoTypeInference()
+
+	// A simple showcase of the any type
+	demoAnyType()
+
+	// Using the any type for unmarshalling dynamic (json) data
+	demoAnyAndUnmarshall()
+}
+
+func demoAnyType() {
+	fmt.Println()
+	fmt.Println("* Any type demo")
+
+	// Following line is same as var anyOne interface{}
+	var anyOne any
+
+	fmt.Println("** Default value")
+	fmt.Printf("Type:  %+T\n", anyOne)
+	fmt.Printf("Value: %+v\n\n", anyOne)
+
+	fmt.Println("** After int assignment")
+	anyOne = 10
+	fmt.Printf("Type:  %+T\n", anyOne)
+	fmt.Printf("Value: %+v\n\n", anyOne)
+
+	fmt.Println("** After string assignment")
+	anyOne = "text"
+	fmt.Printf("Type:  %+T\n", anyOne)
+	fmt.Printf("Value: %+v\n\n", anyOne)
+
+	fmt.Println("** After map assignment")
+	anyOne = make(map[string]int)
+	fmt.Printf("Type:  %+T\n", anyOne)
+	fmt.Printf("Value: %+v\n\n", anyOne)
+
+	fmt.Println("** After slice assignment")
+	anyOne = make([]int, 5)
+	fmt.Printf("Type:  %+T\n", anyOne)
+	fmt.Printf("Value: %+v\n\n", anyOne)
+
+	fmt.Println("** After array assignment")
+	anyOne = [5]int{0, 1, 2, 3, 4}
+	fmt.Printf("Type:  %+T\n", anyOne)
+	fmt.Printf("Value: %+v\n\n", anyOne)
+}
+
+func dumpValue(title string, arg any) {
+	var padding strings.Builder
+
+	for i := 0; i < tableWidth-(len(title)+1); i++ {
+		padding.WriteString(" ")
+	}
+
+	fmt.Printf("%s:%s%+v\n", title, padding.String(), arg)
+}
+
+func dumpType(title string, arg any) {
+	var padding strings.Builder
+
+	for i := 0; i < tableWidth-(len(title)+1); i++ {
+		padding.WriteString(" ")
+	}
+
+	fmt.Printf("%s:%s%+T\n", title, padding.String(), arg)
+}
+
+/*
+** A basic use case for the any type is when unmarshalling json data which are
+** dynamic and cannot be predicted.
+ */
+func demoAnyAndUnmarshall() {
+	fmt.Println()
+	fmt.Println("* Any and unmarshall demo")
+
+	// Following line is same as var anyOne interface{}
+	data := `{"name": "Adam", "age": 33, "skills": ["dreaming", "hunting"], "weapons": ["bag", "pen"]}`
+
+	fmt.Println("** Default json data")
+	dumpValue("Value", data)
+	dumpType("Type", data)
+	fmt.Println()
+
+	var obj any
+	if err := json.Unmarshal([]byte(data), &obj); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("** After unmarshalled data (object)")
+	dumpValue("Value", obj)
+	dumpType("Type", obj)
+	fmt.Println()
+
+	fmt.Println("** Get values as object properties")
+	dumpValue("Name", obj.(map[string]any)["name"])
+	// or a shorter and better way
+	u := obj.(map[string]any)
+	dumpValue("Age", u["age"])
+	dumpValue("Weapons", u["weapons"])
+
+	skills := u["skills"].([]any)
+	dumpValue("Skills", skills)
+	dumpValue("First skill", skills[0])
+	dumpValue("Second skills", skills[1])
 }
 
 func demoTypeInference() {
