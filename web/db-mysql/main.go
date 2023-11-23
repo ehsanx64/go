666ad50670eef78e291ed7c6d9afc6264002bba7
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-11-23 22:19:56 +0330

    web: add example for querying rows (multiple records)

--- web/db-mysql/main.go
+++ web/db-mysql/main.go
@@ -67,6 +67,42 @@ func getUserById(db *sql.DB, userId int) {
 	}
 }
 
+func getAllUsers(db *sql.DB) {
+	type user struct {
+		id        int
+		username  string
+		password  string
+		createdAt time.Time
+	}
+
+	rows, err := db.Query("select * from users")
+	defer rows.Close()
+
+	var users []user
+	if err != nil {
+		log.Println(err)
+	} else {
+		for rows.Next() {
+			var u user
+			err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
+			if err != nil {
+				log.Println("Error getting user record")
+			} else {
+				users = append(users, u)
+			}
+		}
+	}
+
+	fmt.Println("User records:")
+	for index, user := range users {
+		fmt.Println("Index:", index)
+		fmt.Println("Username:", user.username)
+		fmt.Println("Password:", user.password)
+		fmt.Println("The Date:", user.createdAt)
+		fmt.Println()
+	}
+}
+
 func main() {
 	// Open a connection to database
 	db, err := sql.Open("mysql", "goweb:goweb@(127.0.0.1:3306)/goweb?parseTime=true")
@@ -80,4 +116,5 @@ func main() {
 	createTable(db)
 	insertUser(db)
 	getUserById(db, 1)
+	getAllUsers(db)
 }
