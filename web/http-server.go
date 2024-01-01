a73ee8d10b3cd5250e7714dec360c1f20c8bae61
Ehsan Mohammadi <ehsanx64@gmail.com>
2024-01-01 22:05:05 +0330

    web: refactor http-server example

--- web/http-server.go
+++ web/http-server.go
@@ -2,14 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
+
+/*
+** Dump the query string
+ */
+func dumpQuery(r *http.Request) {
+	log.Println("*** Query string dump")
+
+	if len(r.URL.Query()) > 0 {
+		// If map is not empty loop over its keys/values
+		for k, v := range r.URL.Query() {
+			log.Printf("%-5s: %s\n", k, v)
+		}
+	} else {
+		// If map is empty log it
+		log.Println("Query string is empty")
+	}
+}
 
 func main() {
 	// Add root handler
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome ...")
-		fmt.Println("New request (" + r.URL.Path + ") from " + r.RemoteAddr)
+		log.Println("New request (" + r.URL.Path + ") from " + r.RemoteAddr)
+
+		fmt.Fprintf(w, `
+			<a href="http://localhost:8080/?name=adam&age=30">Query</a>
+		`)
+
+		// Try to dump all query string parameters
+		dumpQuery(r)
+
+		// Get a specific parameter
+		log.Println("Name:", r.URL.Query().Get("name"))
 	})
 
 	// Add static file server
