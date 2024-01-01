74a80a7f21bad082deb6b2b05c8ba31c876f13e6
Ehsan Mohammadi <ehsanx64@gmail.com>
2024-01-01 22:19:29 +0330

    web: refactor routing example and add air for auto reloading

--- web/routing/main.go
+++ web/routing/main.go
@@ -7,6 +7,9 @@ import (
 	"github.com/gorilla/mux"
 )
 
+/*
+** Get a book title from route variable and display it
+ */
 func GetBook(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
@@ -14,6 +17,9 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Title: %s\n", vars["title"])
 }
 
+/*
+** Create a new book
+ */
 func AddBook(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
@@ -21,6 +27,9 @@ func AddBook(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Title: %s\n", vars["title"])
 }
 
+/*
+** Update a book
+ */
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
@@ -28,6 +37,9 @@ func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Title: %s\n", vars["title"])
 }
 
+/*
+** Delete a book
+ */
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
@@ -35,6 +47,9 @@ func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Title: %s\n", vars["title"])
 }
 
+/*
+** List all books
+ */
 func ListBooks(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Listing books...\n")
 }
@@ -42,6 +57,7 @@ func ListBooks(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 
+	// A route with two variables
 	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		title := vars["title"]
@@ -52,7 +68,13 @@ func main() {
 
 	// Books path prefix and subrouter
 	bookRouter := r.PathPrefix("/books").Subrouter()
+
+	// Following route match only books/
 	bookRouter.HandleFunc("/", ListBooks).Methods("GET")
+
+	// Following route is added to handle /books
+	bookRouter.HandleFunc("", ListBooks).Methods("GET")
+
 	bookRouter.HandleFunc("/{title}", GetBook).Methods("GET")
 	bookRouter.HandleFunc("/{title}", AddBook).Methods("POST")
 	bookRouter.HandleFunc("/{title}", UpdateBook).Methods("PUT")
