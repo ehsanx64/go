5af436f61a9ab3c79d1ead22d1da3f682aa1d2fd
Ehsan Mohammadi <ehsanx64@gmail.com>
2024-01-23 23:53:12 +0330

    packages: add gorm/postgresql/sql folder and basic scripts

---
+++ packages/gorm/postgresql/sql/products-seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO products (created_at, updated_at, code, price)
+VALUES (now(), now(), 'S43', 1000);
---
+++ packages/gorm/postgresql/sql/products.sql
@@ -0,0 +1,9 @@
+SELECT count(*) AS product_count
+FROM products;
+
+SELECT * 
+FROM products;
+
+SELECT * 
+FROM products 
+WHERE deleted_at IS NULL;
